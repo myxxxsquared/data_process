a9405cbe1a1ad2650c88e703ef5a63e64ec6e203
ruanjiaqiang <randyruan96@gmail.com>
2018-02-03 20:57:26 +0800

    test

--- reader.py
+++ reader.py
@@ -624,10 +624,10 @@ if __name__ == '__main__':
 
 
     print('123')
-    othertext('msra_train/', 10000, 1, True, 'msra')
-    for res in othertext_decoder(TFRECORD_DIR+'msra_train/0_train.tfrecords'):
-        print(res['img'])
-        print(res['contour'])
+    # othertext('msra_train/', 10000, 1, True, 'msra')
+    # for res in othertext_decoder(TFRECORD_DIR+'msra_train/0_train.tfrecords'):
+    #     print(res['img'])
+    #     print(res['contour'])
 
     # patch_num = 35
     # jobs = []
