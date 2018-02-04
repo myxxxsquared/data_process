3f4108d3eb2e271cc0879ae5d733f7455eaf56fd
JiaqiangRuan <randyruan96@gmail.com>
2018-02-05 00:42:53 +0800

    check get maps

--- utils.py
+++ utils.py
@@ -687,7 +687,7 @@ if __name__ == '__main__':
     PKL_DIR = '/home/rjq/data_cleaned/pkl/'
     import pickle
 
-    for i in range(10):
+    for i in range(9, 10):
         res = pickle.load(open(PKL_DIR+'totaltext_train/'+str(i)+'.bin', 'rb'))
         print(res['img_name'],
               res['contour'],
