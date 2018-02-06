f3c26628e69c2fec499fc64c3c7294284081092e
ruanjiaqiang <randyruan96@gmail.com>
2018-02-06 17:19:06 +0800

    fix bug on theta

--- utils.py
+++ utils.py
@@ -307,8 +307,8 @@ def find_mid_line_with_radius_theta(points_list, crop_skel, neighbor, sampling_n
             theta_dict[point] = theta
     else:
         for p,point in enumerate(center_line):
-            neighbor_num = int(round(neighbor * radius_dict[point]))
-            theta = get_theta(list(set(center_line[max(p-neighbor_num,0):p+neighbor_num])))
+            neighbor = int(round(neighbor))
+            theta = get_theta(list(set(center_line[max(p-neighbor,0):p+neighbor])))
             theta_dict[point] = theta
 
     temp = []
@@ -650,7 +650,7 @@ if __name__ == '__main__':
         is_text_cnts = res['is_text_cnts']
 
         skels_points, radius_dict, score_dict, cos_theta_dict, sin_theta_dict, mask_fills = \
-            get_maps(img, cnts, is_text_cnts, thickness=0.15, crop_skel=1.0, neighbor=0.5)
+            get_maps(img, cnts, is_text_cnts, thickness=0.15, crop_skel=1.0, neighbor=5)
         TR = mask_fills[0]
         for i in range(1, len(mask_fills)):
             TR = np.bitwise_or(TR, mask_fills[i])
@@ -711,7 +711,7 @@ if __name__ == '__main__':
     #     cv2.imwrite(img_name+'_box.jpg', zeros)
     #
     #     skels_points, radius_dict, score_dict, cos_theta_dict, sin_theta_dict, mask_fills = \
-    #         get_maps(img, cnts, is_text_cnts, thickness=0.15, neighbor=1.0, crop_skel=1.0,
+    #         get_maps(img, cnts, is_text_cnts, thickness=0.15, neighbor=5, crop_skel=1.0,
     #                  chars = chars)
     #     TR = mask_fills[0]
     #     for i in range(1, len(mask_fills)):
