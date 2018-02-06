df0f9535fadb8bc0b63416a60351be47367e3f2a
ruanjiaqiang <randyruan96@gmail.com>
2018-02-06 17:15:50 +0800

    fix bug on theta

--- utils.py
+++ utils.py
@@ -638,7 +638,7 @@ if __name__ == '__main__':
     PKL_DIR = '/home/rjq/data_cleaned/pkl/'
     import pickle
 
-    for i in range(9, 10):
+    for i in range(9, 15):
         res = pickle.load(open(PKL_DIR+'totaltext_train/'+str(i)+'.bin', 'rb'))
         print(res['img_name'],
               res['contour'],
@@ -650,7 +650,7 @@ if __name__ == '__main__':
         is_text_cnts = res['is_text_cnts']
 
         skels_points, radius_dict, score_dict, cos_theta_dict, sin_theta_dict, mask_fills = \
-            get_maps(img, cnts, is_text_cnts, thickness=0.15, crop_skel=1.0, neighbor=0.3)
+            get_maps(img, cnts, is_text_cnts, thickness=0.15, crop_skel=1.0, neighbor=0.5)
         TR = mask_fills[0]
         for i in range(1, len(mask_fills)):
             TR = np.bitwise_or(TR, mask_fills[i])
